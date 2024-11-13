30075c1861eaa5ed10db54f02d65a72402901ddb
Rubén Sánchez González <36800806+RubnSanchz@users.noreply.github.com>
2024-11-13 16:16:07 +0000

    fat: add help messages

--- utilities/gpg_encrypt.mjs
+++ utilities/gpg_encrypt.mjs
@@ -1,9 +1,9 @@
 // Encryption: gpg --batch --passphrase "$GPG_PASSWORD" --symmetric --cipher-algo AES256 <route_to_file>
+// *** remember to set the GPG_PASSWORD environment variable. Then export  GPG_PASSWORD
 // Encryption usage: node gpg_encrypt.mjs -f <route_to_file> [-d <bool_delete_source_file>]
 
 import { exec } from 'child_process'
 import { promisify } from 'util'
-import path from 'path'
 import yargs from 'yargs/yargs'
 import { hideBin } from 'yargs/helpers'
 const execAsync = promisify(exec)
@@ -23,12 +23,15 @@ const argv = yargs(hideBin(process.argv))
     requiresArg: false,
     required: false,
   })
+  .help('help')
+  .alias('help', 'h')
+  .example('$0 -f /path/to/file.csv', 'Encrypt the specified file with GPG AES256')
+  .example('$0 -f /path/to/file.csv -d', 'Encrypt the specified file and delete the source file')
   .parseSync()
 
 // Function to encrypt a file
 async function encryptFile(encryptedFilePath) {
   const passphrase = process.env.GPG_PASSWORD
-
   if (!passphrase) {
     throw new Error('GPG_PASSWORD environment variable is not set.')
   }
